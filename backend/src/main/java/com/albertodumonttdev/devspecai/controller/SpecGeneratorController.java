f8fce8e9a98e3668cfb7b3b5a8ff606671e0c262
Alberto Dumontt <albertodumonttdev@outlook.com>
2025-06-14 14:54:15 -0300

    feat: add urls

--- backend/src/main/java/com/albertodumonttdev/devspecai/controller/SpecGeneratorController.java
+++ backend/src/main/java/com/albertodumonttdev/devspecai/controller/SpecGeneratorController.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/spec")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {
+        "http://localhost:5173",
+        "https://devspecai.onrender.com"
+})
 public class SpecGeneratorController {
 
     @Autowired
